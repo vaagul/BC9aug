f9fa4d3a9d7966da1eff5c3727412c15a61e503a
vaagul <vaagul@gmail.com>
2018-08-09 13:29:24 +0530

    added helper functions

--- src/Odometer.java
+++ src/Odometer.java
@@ -10,6 +10,23 @@ public class Odometer {
         //code for updating map
     }
 
+
+
+    private int getKeyfromValue(int value){
+        for(Map.Entry<Integer,Integer> entry : odometerValues.entrySet()){
+            if(entry.getValue() == value)
+                return entry.getKey();
+        }
+        return -1;
+    }
+    private int getValuefromKey(int key){
+        for(Map.Entry<Integer,Integer> entry : odometerValues.entrySet()){
+            if(entry.getKey() == key){
+                return entry.getValue();
+            }
+        }
+        return -1;
+    }
     public int getNext(int currValue,int offset){
         return 0;
     }
