d6ad20f1a5e732218de5c9c84dfb1512f0b43e49
agireesan <anuroop157@gmail.com>
2018-08-09 14:05:18 +0530

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Odometer odometer= new Odometer(4);
-        odometer.getNext(0,0);
-
+        //odometer.printMap();
+        System.out.println(odometer.getImmediateNext(6789));
+        System.out.println(odometer.getImmediatePrevious(1234));
+        System.out.println(odometer.getNext(3456,10));
+        System.out.println(odometer.getDifference(1234,6789));
+        System.out.println(odometer.getPrevious(2345,1));
     }
 }
--- src/Odometer.java
+++ src/Odometer.java
@@ -8,23 +8,28 @@ public class Odometer {
     Odometer(int digits){
         this.digits=digits;
         int key=0;
-        for(int i= (int)Math.pow(10,digits-1); i< (int)Math.pow(10,digits)-1;i++){
-            if(isValidDigit(i))
-                odometerValues.put(key++,i);
+        for(int i= (int)Math.pow(10,digits-1); i< (int)Math.pow(10,digits);i++){
+            if(isValidDigit(i)) {
+                odometerValues.put(key++, i);
+            }
         }
-
     }
-    private boolean isValidDigit(int n){
+    public boolean isValidDigit(int n){
         boolean is_ascending=true;
         while(n>9){
             int temp = (n/10);
-            if(n%10 <= temp%10);
+            if(n%10 <= temp%10)
                 is_ascending=false;
             n=n/10;
         }
         return is_ascending;
     }
 
+    public void printMap(){
+        for(Map.Entry<Integer,Integer> entry:odometerValues.entrySet()){
+            System.out.println(entry.getKey() + "\t" +entry.getValue());
+        }
+    }
 
 
     private int getKeyfromValue(int value){
@@ -43,19 +48,25 @@ public class Odometer {
         return -1;
     }
 
+    public int getImmediateNext(int currvalue){
+        return getNext(currvalue,1);
+    }
+
+    public int getImmediatePrevious(int currvalue){
+        return getPrevious(currvalue,1);
+    }
+
      public int getNext(int currValue,int offset){
     	int size=odometerValues.size();
-    	int value;
-    	int index=getKeyfromValue(currValue);
-    	value=getValuefromKey(index+offset)%size;
-    	return value;
+    	int key=getKeyfromValue(currValue);
+    	return getValuefromKey((key+offset)%size);
+
     }
 	public int getPrevious(int currValue,int offset){
 		int size=odometerValues.size();
-    	int value;
     	int index=getKeyfromValue(currValue);
-    	value=getValuefromKey(index-offset+size)%size;
-    	return value;
+    	return getValuefromKey((index-offset+size)%size);
+
     }
 
     public int getDifference(int n1,int n2){
